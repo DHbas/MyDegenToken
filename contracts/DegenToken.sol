7ea9a1f9346fbd106212aadebe79b57d97d61d28
David Bas <76508297+DHbas@users.noreply.github.com>
2023-11-21 21:52:08 +0800

    UPDATE DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -1,14 +1,46 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "hardhat/console.sol";
 
-contract DegenToken is ERC20, Ownable {
+contract DegenToken is ERC20, Ownable, ERC20Burnable {
+    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {}
 
-    constructor() ERC20("Degen", "DGN") {}
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount); 
+    }
 
-        function mint(address to, uint256 amount) public onlyOwner {
-            _mint(to, amount);
+    function checkBalance(address account) public view returns (uint256) {
+        return balanceOf(account);
+    }
+
+    function transferTokens(address _receiver, uint256 _value) external {
+        require(balanceOf(msg.sender) >= _value, "You do not have enough Degen Tokens");
+        _transfer(msg.sender, _receiver, _value);
+    }
+
+    function burnTokens(uint256 _value) external {
+        require(balanceOf(msg.sender) >= _value, "You do not have enough Degen Tokens");
+        _burn(msg.sender, _value);
+    }
+
+    function redeemTokens(uint8 _userChoice) external payable returns (bool) {
+        if (_userChoice == 1) {
+            require(balanceOf(msg.sender) >= 10, "You do not have enough Degen Tokens");
+            _transfer(msg.sender, owner(), 10);
+            console.log("Redeemed 10 Degen Tokens");
+            return true;
+        } else if (_userChoice == 2) {
+            require(balanceOf(msg.sender) >= 5, "You do not have enough Degen Tokens");
+            _transfer(msg.sender, owner(), 5);
+            console.log("Redeemed 5 Degen Tokens");
+            return true;
+        } else {
+            console.log("That is not a valid choice");
+            return false;
+        }
     }
 }
